07b64ffa08cb627057153cdd8103ed20c0e1b5dd
jliu14 <jean.liu@morningstar.com>
2019-12-25 18:20:08 +0800

    optimize tag description

--- webmagic-samples/src/main/java/us/codecraft/webmagic/model/samples/parseHtml/JsoupParse.java
+++ webmagic-samples/src/main/java/us/codecraft/webmagic/model/samples/parseHtml/JsoupParse.java
@@ -15,6 +15,13 @@ public class JsoupParse {
     //3.格式规整html文件，生成只包含 title 标题的h3标签, p 内容标签，p中可以包含keyword关键字strong标签， table表格标签， img 图片标签的html文件
     //4.读取规整后的html文件，判断节点内容（通过规则和机器学习来判断内容），并打上数据点标签， 同时生成对应的json文件
     //5.将规整后html文件的打标内容同步到原始html文档中
+    /*
+    示例：
+        <h3 uid='abc123456' datapointname='fundName' investmentid='f000001'>MainStay Short Term Bond Fund</h3>
+        <h3 uid='abc123457' datapointname='investmentObjective' investmentid='f000001'>Investment Objective</h3>
+        <p uid='abc123458' datapointname='investmentObjective' >The Cannabis ETF (the “Fund”) seeks to provide investment results that, before fees and expenses, correspond generally to the total return performance of the Innovation Labs Cannabis Index</p>
+
+     */
 
     private static String[] lineTagsArray = new String[]{"a","abbr","acronym","b","bdo","big","br","cite","code","dfn",
                                "em","font","i","img","input","kbd","label","q","s","samp",
