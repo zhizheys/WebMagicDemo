b69695fc124343e27479c054718117f9a85ac147
testme <testme@126.com>
2020-02-08 21:51:50 +0800

    add open nlp

--- webmagic-samples/src/main/java/us/codecraft/webmagic/model/samples/TestConsole.java
+++ webmagic-samples/src/main/java/us/codecraft/webmagic/model/samples/TestConsole.java
@@ -8,6 +8,7 @@ import us.codecraft.webmagic.model.samples.parseHtml.HttpUtil;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 
 public class TestConsole {
@@ -15,53 +16,19 @@ public class TestConsole {
     static   String a;
 
     public static  void main(String[] args){
-         a="abc";
-        System.out.println(a);
 
-        //a="def";
-
-        System.out.println(a);
+        for(int j=0;j<100;j++){
+            Random random = new Random();
+            Integer a =random.nextInt(3);
+            System.out.println(a);
+        }
 
         System.out.println("end");
     }
 
     public static void showElement(Element element){
 
-        String text = element.text();
-        String html = element.html();
-        String outerHtml = element.outerHtml();
-        String tag = element.tagName();
 
-        //String classInfo=element.attr("class");
-       // String styleInfo = element.attr("style");
-
-
-        System.out.println("----------------------");
-        System.out.println("current text: " + text);
-        System.out.println("current html: " + html);
-        System.out.println("current outer html: " + outerHtml);
-
-        element.attr("nodeid","1234567a");
-        element.tagName("strong");
-        System.out.println("current outer html: " + element.outerHtml());
-        System.out.println("=============================");
-
-
-        if("head".equalsIgnoreCase(tag)){
-            return;
-        }
-
-        Elements  sub = element.children();
-
-        if(sub.size() >0){
-            System.out.println("size is: " + sub.size());
-            for(Element item : sub){
-                showElement(item);
-            }
-        }else{
-
-            System.out.println("no child ++++++++++" + sub.size());
-        }
 
 
     }
